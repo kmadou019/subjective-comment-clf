78120e907978e7cf87a1071a5ae90858cbac2be3
madou <kmadou019@gmail.com>
2025-02-13 17:08:46 +0100

    The collection is well created I just have to give the good function of embedding

--- chroma.py
+++ chroma.py
@@ -5,26 +5,27 @@ import pandas as pd
 from mistralai import Mistral
 from chromadb.utils import embedding_functions
 
-API_KEY = "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"
-huggingface_ef = embedding_functions.HuggingFaceEmbeddingFunction(
-    api_key=API_KEY,
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+#The embedding function
+emb_fn = embedding_functions.DefaultEmbeddingFunction()
 #Loading data set
 data = pd.read_csv("comments.csv")
 #Merging comment and tag
 comments = [f"""{content} : {tag} """ for content,tag in zip(data["content"], data["tag"])]
 
-#Getting embedding of each comment
-embeddings = huggingface_ef(comments[1])
-
 #Chromadb creation
-
 chroma_client = chromadb.PersistentClient("./chroma_db")
 
-collection = chroma_client.get_or_create_collection(name="Comment")
-collection.add(
-    ids=data["id"],
-    embeddings=embeddings
+collection = chroma_client.get_or_create_collection(
+    name="Comment",
+    embedding_function=emb_fn,
+    metadata={
+        "hnsw:space":"cosine",
+        "hnsw:search_ef":10
+    })
+
+
+collection.upsert(
+    documents=comments,
+    ids=data["id"].astype(str).tolist()
 )
 
