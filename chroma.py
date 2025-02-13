ee2581b8900284bb20cd3b543a1af549079d6256
madou <kmadou019@gmail.com>
2025-02-13 16:17:38 +0100

    pause

--- chroma.py
+++ chroma.py
@@ -3,25 +3,20 @@
 import chromadb
 import pandas as pd
 from mistralai import Mistral
+from chromadb.utils import embedding_functions
 
-API_KEY = "YDjpRuQdjQDiE2z0hjBfn0G65TYRg80i"
-
-def get_embeddings_by_chunks(data, chunk_size):
-    chunks = [data[x : x + chunk_size] for x in range(0, len(data), chunk_size)]
-    embeddings_response = [
-        client.embeddings.create(model=model, inputs=c) for c in chunks
-    ]
-    return [d.embedding for e in embeddings_response for d in e.data]
-
+API_KEY = "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"
+huggingface_ef = embedding_functions.HuggingFaceEmbeddingFunction(
+    api_key=API_KEY,
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
 #Loading data set
 data = pd.read_csv("comments.csv")
 #Merging comment and tag
 comments = [f"""{content} : {tag} """ for content,tag in zip(data["content"], data["tag"])]
 
 #Getting embedding of each comment
-model = "mistral-embed"
-client = Mistral(api_key=API_KEY)
-embeddings = get_embeddings_by_chunks(comments, 50)
+embeddings = openai_ef(comments[1])
 
 #Chromadb creation
 
