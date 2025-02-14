540f7770cca75fde294507334099d38e09ffe88a
madou <kmadou019@gmail.com>
2025-02-14 16:07:23 +0100

    first good

--- rag.py
+++ rag.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import getpass
+import os
 from langchain.chat_models import init_chat_model
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.schema import Document
@@ -9,7 +11,11 @@ from typing_extensions import List, TypedDict
 import pandas as pd
 
 
+if "MISTRAL_API_KEY" not in os.environ:
+    os.environ["MISTRAL_API_KEY"] = getpass.getpass("Enter your Mistral API key: ")
+
 llm = init_chat_model("mistral-large-latest", model_provider="mistralai")
+
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 vector_store = Chroma(embedding_function=embeddings)
 
@@ -21,20 +27,41 @@ comments = [Document(f"""{content} : {tag} """) for content,tag in zip(data["con
 #Store the comments in the chromadb by indexing them
 vector_store.add_documents(documents=comments)
 
+
+
 # Define prompt for classification
 template = """
-Tu es un expert en classification de commentaire. 
-Voici un commentaire : 
-{comment}
+Tu es un expert en classification de commentaires. 
 
-Classe ce commentaire dans l'une des catégories suivantes :
-1. ...
-2. ...
 
-En te basant sur ces classifications existantes (commentaire : classe):
+Voici des catégories dont les definitions sont données :
+1.MonCal.Facts.RF : Observations factuelles sur les résultats du test, incluant des remarques sur le nombre/quantité de réponses correctes et incorrectes.
+
+2.MonCal.Facts.DC : Observations factuelles sur les résultats du test, incluant des remarques sur la quantité de connaissances fragiles, de connaissances certaines, d'erreurs dangereuses et d'erreurs présumées.
+
+3.MonCal.Interpret : Interprétations des résultats du test allant au-delà des observations factuelles, reflétant les interprétations subjectives des étudiants sur leur performance.
+
+4.BDS.Emotions : Commentaires exprimant les émotions ressenties par l’étudiant pendant et après le test.
+
+5.MotOrient.Value : Commentaires sur l'intérêt pour les types de tests (QCM, QCM avec DC) et leur valeur perçue pour l’apprentissage.
+
+6.MotOrient.SelfEff : Accent sur l’auto-efficacité et la confiance des étudiants dans le domaine évalué.
+
+7.DomainKldg : Commentaires identifiant les concepts/disciplines manquants et leur degré d’acquisition.
+
+8.StratKldg : Commentaires sur les stratégies d’apprentissage utilisées pendant le test et l’analyse du comportement des étudiants.
+
+9.CTRL : Commentaires sur les comportements futurs des apprenants.
+
+
+En te basant sur  les commentaires suivants classifiés par un expert humain donnés sous le format (commentaire : classe):
 {context}
 
-Classe du commentaire : 
+donne la classe de ce commentaire : 
+{comment}
+
+donne ta reponse sous ce format:
+classe : <classe>
 """
 
 prompt = PromptTemplate(
