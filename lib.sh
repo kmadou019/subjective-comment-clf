6148a1f15b5b8bb8be9ce8f332e4c8dad9f3a0c8
madou <kmadou019@gmail.com>
2025-02-18 11:05:38 +0100

    lib

---
+++ lib.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pip install langchain
+pip install langchain_huggingface
+pip install langchain_chroma
+pip install langgraph
+pip install typing_extensions
+pip install pandas
+pip install langchain_ollama
+pip install chromadb
