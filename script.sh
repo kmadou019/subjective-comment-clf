89454f120b4dd17aa1880c019f819b82780b953b
madou <kmadou019@gmail.com>
2025-02-25 20:12:12 +0100

    Clean up

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#OAR -n rag
-#OAR --stdout log/output.txt
-#OAR --stderr log/error.txt
-#OAR -l /host=1/gpu=4,walltime=7:0:0
-source mon_env/bin/activate
-/home/daisy/konema/Documents/ollama/bin/ollama ps > running.oll
-/home/daisy/konema/Documents/ollama/bin/ollama serve &
-./rag.py
