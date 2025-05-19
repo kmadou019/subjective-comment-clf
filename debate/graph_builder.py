6d3157076d238aa2c37ef0efec7ab2c1e91ee00e
konema <konema@lig-gpu9.imag.fr>
2025-05-19 12:04:04 +0200

    let's return back to the previous model

--- debate/graph_builder.py
+++ debate/graph_builder.py
@@ -5,26 +5,18 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from typing_extensions import TypedDict
 import logging
-import json
 import ast
 MAX_TURN = 4
 
 #Define agents
 name_orchestrator = "mistral"
-name_debater1 = "phi4"
-name_debater2 = "llama3.3"
+name_debater1 = "mistral"
+name_debater2 = "mistral"
 # Initialize the LLMs
-model_orchestrator =  OllamaLLM(model=name_orchestrator)
-model_debater1 = OllamaLLM(model=name_debater1)
-model_debater2 = OllamaLLM(model=name_debater2)
+orchestrator =  OllamaLLM(model=name_orchestrator)
+debater1 = OllamaLLM(model=name_debater1)
+debater2 = OllamaLLM(model=name_debater2)
 #Intitialize memories
-memory_orchestrator = ConversationBufferMemory()
-memory_debater1     = ConversationBufferMemory()
-memory_debater2     = ConversationBufferMemory()
-
-orchestrator = ConversationChain(llm=model_orchestrator, memory = memory_orchestrator)
-debater1     = ConversationChain(llm=model_debater1,     memory = memory_debater1)
-debater2     = ConversationChain(llm=model_debater2,     memory = memory_debater2)
 
 #Intialize the log file
 log_file = "debate_log.txt"
@@ -51,60 +43,8 @@ class State(TypedDict):
     debater2_response: dict
     final_evaluation: str
 
-def Orchestrator(state: State):
-    if state['turn'] == 0:
-        prompt = f"""
-        Vous êtes un orchestrateur supervisant un débat entre deux classificateurs de commentaire d'étudiants.
-        Le commentaire à classifier est le suivant : {state['comment']}
-
-        il doit être classifier dans l'une des neufs classes suivantes dont la description est également fournie : 
-
-        MonCal.Interpret : Interprétations des résultats du test allant au-delà des observations factuelles, reflétant les interprétations subjectives des étudiants sur leur performance.
-
-        MonCal.Facts.RF : Observations factuelles sur les résultats du test, incluant des remarques sur le nombre/quantité de réponses correctes et incorrectes.
-
-        MonCal.Facts.DC : Observations factuelles sur les savoirs fragiles, savoirs certains, erreurs dangereuses et erreurs présumées.
-
-        BDS.Emotions : Commentaires exprimant les émotions ressenties par l’étudiant pendant et après le test.
-
-        MotOrient.Value : Commentaires sur l'intérêt des étudiants pour les types de tests et leur valeur perçue pour l’apprentissage.
-
-        MotOrient.SelfEff : Commentaires sur la confiance et l’auto-efficacité des étudiants.
-
-        DomKldg : Commentaires identifiant les concepts/disciplines manquants et leur degré d’acquisition.
-
-        StratKldg : Commentaires sur les stratégies d’apprentissage utilisées pendant le test et l’analyse du comportement des étudiants.
-
-        CTRL : Commentaires sur les comportements futurs des apprenants.
-
-        Ton but sera de vérifier s'il y a un consensus entre les debatteurs.
-
-        """
-        orchestrator.invoke(prompt)["response"]
-        return {"agreement": False, "turn": state['turn'] + 1}
-    else:
-        prompt = f"""
-        Voici ce qu'à dit le classificateur 1:
-        {state["debater1_response"]}
-        et voici ce qu'à dit le classificateur 2
-        {state["debater2_response"]}
-
-        fait un résumé de la situation, et dit nous s'il y a un consensus entre les classificateurs ou pas. 
-        Veuillez bien à ce que ta reponse soit dans le format suivant(Ecrit bien True/False avec la première lettre en maj sans quotes):
-                
-        {{
-            "turn": {state['turn']},
-            "agreement": "True/False",
-            "summary": "..."
-        }}
-
-        """
-        response = extract_json(orchestrator.invoke(prompt)["response"])
-        #logger.info(f"Résumé du débat par l'orchestrateur ({name_orchestrator}) : {response['summary']}")
-        logger.info("\n")
-        return {"agreement": response["agreement"], "turn": state["turn"]+1}
-
 def Debater1(state: State):
+
     if state['turn'] == 1:
         prompt = f"""
         Tu es un expert en classification de commentaires, tu es engagé dans un debat avec un autre classificateur dans le but de trouver la meilleure classification.
@@ -237,6 +177,59 @@ def Debater2(state: State):
     logger.info(f"Réponse du classificateur 2 ({name_debater2}) : {response}\n")
     return {"debater2_response": response }
 
+def Orchestrator(state: State):
+    if state['turn'] == 0:
+        prompt = f"""
+        Vous êtes un orchestrateur supervisant un débat entre deux classificateurs de commentaire d'étudiants.
+        Le commentaire à classifier est le suivant : {state['comment']}
+
+        il doit être classifier dans l'une des neufs classes suivantes dont la description est également fournie : 
+
+        MonCal.Interpret : Interprétations des résultats du test allant au-delà des observations factuelles, reflétant les interprétations subjectives des étudiants sur leur performance.
+
+        MonCal.Facts.RF : Observations factuelles sur les résultats du test, incluant des remarques sur le nombre/quantité de réponses correctes et incorrectes.
+
+        MonCal.Facts.DC : Observations factuelles sur les savoirs fragiles, savoirs certains, erreurs dangereuses et erreurs présumées.
+
+        BDS.Emotions : Commentaires exprimant les émotions ressenties par l’étudiant pendant et après le test.
+
+        MotOrient.Value : Commentaires sur l'intérêt des étudiants pour les types de tests et leur valeur perçue pour l’apprentissage.
+
+        MotOrient.SelfEff : Commentaires sur la confiance et l’auto-efficacité des étudiants.
+
+        DomKldg : Commentaires identifiant les concepts/disciplines manquants et leur degré d’acquisition.
+
+        StratKldg : Commentaires sur les stratégies d’apprentissage utilisées pendant le test et l’analyse du comportement des étudiants.
+
+        CTRL : Commentaires sur les comportements futurs des apprenants.
+
+        Ton but sera de vérifier s'il y a un consensus entre les debatteurs.
+
+        """
+        orchestrator.invoke(prompt)["response"]
+        return {"agreement": False, "turn": state['turn'] + 1}
+    else:
+        prompt = f"""
+        Voici ce qu'à dit le classificateur 1:
+        {state["debater1_response"]}
+        et voici ce qu'à dit le classificateur 2
+        {state["debater2_response"]}
+
+        fait un résumé de la situation, et dit nous s'il y a un consensus entre les classificateurs ou pas. 
+        Veuillez bien à ce que ta reponse soit dans le format suivant(Ecrit bien True/False avec la première lettre en maj sans quotes):
+                
+        {{
+            "turn": {state['turn']},
+            "agreement": "True/False",
+            "summary": "..."
+        }}
+
+        """
+        response = extract_json(orchestrator.invoke(prompt)["response"])
+        #logger.info(f"Résumé du débat par l'orchestrateur ({name_orchestrator}) : {response['summary']}")
+        logger.info("\n")
+        return {"agreement": response["agreement"], "turn": state["turn"]+1}
+    
 def end(state: State):
     if state['agreement'] == True or (state['turn'] == MAX_TURN and state['agreement'] == False):
         return "last_action"
@@ -276,18 +269,20 @@ def last_action(state: State):
     logger.info(f"Résumé final du débat par l'orchestrateur ({name_orchestrator}) : {final_evaluation}\n")
     return {"agreement": True, "turn": state['turn'], "final_evaluation": final_evaluation}
 
+def RagAgent():
+    ...
 
 graph_builder = StateGraph(State)
 graph_builder.add_node("Orchestrator", Orchestrator)
 graph_builder.add_node("debater1", Debater1)
 graph_builder.add_node("debater2", Debater2)
-graph_builder.add_node("last_action", last_action)
+graph_builder.add_node("RagAgent", RagAgent)
 
-graph_builder.add_edge(START, "Orchestrator")
-graph_builder.add_edge("debater1", "Orchestrator")
-graph_builder.add_edge("debater2", "Orchestrator")
-graph_builder.add_edge("last_action", END)
-graph_builder.add_conditional_edges("Orchestrator", end,["debater1","debater2","last_action"])
+graph_builder.add_edge(START, "debater1")
+graph_builder.add_edge("debater1", "debater2")
+graph_builder.add_edge("debater2", "debater1")
+graph_builder.add_conditional_edges("debater2", end,["Orchestrator"])
+graph_builder.add_conditional_edges("Orchestrator", end,["RagAgent",END])
 graph = graph_builder.compile()
 
 if __name__=="__main__":
