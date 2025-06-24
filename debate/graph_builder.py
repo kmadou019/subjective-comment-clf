8c3260a868d26f89e9ebf1c6b654ec227dae8567
konema <konema@lig-gpu10.imag.fr>
2025-06-25 00:45:29 +0200

    debate hybrid, mistral comme orchestrateur

--- debate/graph_builder.py
+++ debate/graph_builder.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
 from langchain_ollama.llms import OllamaLLM
-from langgraph.graph import START,END, StateGraph
+from langgraph.graph import START, END, StateGraph
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 from typing_extensions import TypedDict
 import logging
 import ast
 from rag.graph_builder import graph as RAG
+
 MAX_TURN = 4
 
-#Define agents
-name_orchestrator = "mistral"
+# Define agents
+name_orchestrator = "phi4"
 name_debater1 = "phi4"
-name_debater2 = "llama3.3"
+name_debater2 = "phi4"
+
 # Initialize the LLMs
-orchestrator =  OllamaLLM(model=name_orchestrator)
-debater1 = OllamaLLM(model=name_debater1)
-debater2 = OllamaLLM(model=name_debater2)
-#Intitialize memories
+orchestrator_llm = OllamaLLM(model=name_orchestrator)
+debater1_llm = OllamaLLM(model=name_debater1)
+debater2_llm = OllamaLLM(model=name_debater2)
 
+# Initialize memories and conversation chains
+memory_orchestrator = ConversationBufferMemory(return_messages=True)
+memory_debater1 = ConversationBufferMemory(return_messages=True)
+memory_debater2 = ConversationBufferMemory(return_messages=True)
 
-#Intialize the log file
+orchestrator = ConversationChain(llm=orchestrator_llm, memory=memory_orchestrator)
+debater1 = ConversationChain(llm=debater1_llm, memory=memory_debater1)
+debater2 = ConversationChain(llm=debater2_llm, memory=memory_debater2)
+
+# Initialize the log file
 log_file = "debate_log.txt"
 logger = logging.getLogger(__name__)
 handler = logging.FileHandler(log_file, mode="a")
 logger.setLevel(logging.DEBUG)
-
 handler.setLevel(logging.INFO)
 handler.addFilter(lambda record: record.levelno == logging.INFO)
 logger.addHandler(handler)
@@ -51,7 +59,7 @@ def Orchestrator(state: State):
         Vous êtes un orchestrateur supervisant un débat entre deux classificateurs de commentaire d'étudiants.
         Le commentaire à classifier est le suivant : {state['comment']}
 
-        il doit être classifier dans l'une des neufs classes suivantes dont la description est également fournie : 
+        Il doit être classifié dans l'une des neufs classes suivantes :
 
         MonCal.Interpret : Interprétations des résultats du test allant au-delà des observations factuelles, reflétant les interprétations subjectives des étudiants sur leur performance.
 
@@ -71,103 +79,92 @@ def Orchestrator(state: State):
 
         CTRL : Commentaires sur les comportements futurs des apprenants.
 
-        Ton but sera de vérifier s'il y a un consensus entre les debatteurs.
-
+        Ton but sera de vérifier s'il y a un consensus entre les débatteurs.
         """
-        orchestrator.invoke(prompt)
+        orchestrator.predict(input=prompt)
         return {"agreement": False, "turn": state['turn'] + 1}
     else:
         prompt = f"""
-        Voici ce qu'à dit le classificateur 1:
+        Voici ce qu'a dit le classificateur 1:
         {state["debater1_response"]}
-        et voici ce qu'à dit le classificateur 2
+        Et voici ce qu'a dit le classificateur 2:
         {state["debater2_response"]}
 
-        fait un résumé de la situation, et dit nous s'il y a un consensus entre les classificateurs ou pas. 
-        Veuillez bien à ce que ta reponse soit dans le format suivant(Ecrit bien True/False avec la première lettre en maj sans quotes):
-                
+        Fait un résumé de la situation, et dis-nous s'il y a un consensus entre les classificateurs ou pas.
+
+        Veuillez bien à ce que ta réponse soit dans le format suivant (Écrit bien True/False avec la première lettre en maj sans quotes) :
+
         {{
             "turn": {state['turn']},
-            "agreement": "True/False",
+            "agreement":"True/False",
             "summary": "..."
         }}
-
         """
-        response = extract_json(orchestrator.invoke(prompt))
+        response = extract_json(orchestrator.predict(input=prompt))
         print("Orchestrator : ", response)
         logger.info("\n")
         return {"agreement": response["agreement"], "turn": state["turn"]+1}
 
 def Debater1(state: State):
-    response = ""
     if state['turn'] == 1:
-        response = extract_json(RAG.invoke({"comment":state["comment"], "llm" : name_debater1})["predictedClass"])
+        response = extract_json(RAG.invoke({"comment": state["comment"], "llm": name_debater1})["predictedClass"])
         logger.info(f"Réponse du classificateur 1 ({name_debater1}) (RAG) : {response}\n")
     else:
         prompt = f"""
-        Dans le tour précédent du débat, tu as donné la classe suivante : 
+        Dans le tour précédent du débat, tu as donné la classe suivante :
         {state['debater1_response']["classe"]}
 
-        Cependant, le second classificateur a exprimé un désaccord. Voici sa réponse : 
+        Cependant, le second classificateur a exprimé un désaccord. Voici sa réponse :
         {state['debater2_response']}
 
-        Réexamine ta position à la lumière de sa justification. 
+        Réexamine ta position à la lumière de sa justification.
 
-        Sois confiant dans ton raisonnement initial, mais reste **ouvert à la discussion** : si l’autre classificateur apporte des **arguments solides** ou met en lumière une **faiblesse réelle** dans ta réponse, n’hésite pas à adapter ta position.
-
-        Ne modifie pas ta réponse sans raison valable, mais sois prêt à reconnaître une meilleure justification si elle est clairement fondée.
-
-        Quelle que soit ta décision — maintien ou changement de classe — explique ton raisonnement de manière claire et rigoureuse.
+        Sois confiant dans ton raisonnement initial, mais reste **ouvert à la discussion** : si l’autre classificateur apporte des **arguments solides**, n’hésite pas à adapter ta position.
 
         Ta réponse doit être fournie dans le format JSON suivant :
         {{
             "classe": "...",
             "justification": "..."
         }}
-        Soit bref dans ta justification.
+        Sois bref dans ta justification.
         """
-        response = extract_json(debater1.invoke(prompt))
+        response = extract_json(debater1.predict(input=prompt))
         logger.info(f"Réponse du classificateur 1 ({name_debater1}) : {response}\n")
-       
-    print("Debator 1 :", response)
-    return {"debater1_response": response }
+
+    print("Debater 1 :", response)
+    return {"debater1_response": response}
 
 def Debater2(state: State):
-    response = ""
     if state['turn'] == 1:
-        response = extract_json(RAG.invoke({"comment":state["comment"], "llm": name_debater2})["predictedClass"])
+        response = extract_json(RAG.invoke({"comment": state["comment"], "llm": name_debater2})["predictedClass"])
         logger.info(f"Réponse du classificateur 2 ({name_debater2}) (RAG) : {response}\n")
     else:
         prompt = f"""
-        Dans le tour précédent du débat, tu as donné la classe suivante : 
+        Dans le tour précédent du débat, tu as donné la classe suivante :
         {state['debater2_response']["classe"]}
 
-        Cependant, le second classificateur a exprimé un désaccord. Voici sa réponse : 
+        Cependant, le second classificateur a exprimé un désaccord. Voici sa réponse :
         {state['debater1_response']}
 
         Réexamine brièvement ta propre réponse à la lumière de cet avis contradictoire.
 
-        Attention : 
-        Tu dois rester **ferme et confiant** dans ta décision initiale, sauf si les arguments adverses sont **clairement fondés et plus convaincants que les tiens**.
+        Attention :
+        Tu dois rester **ferme et confiant** dans ta décision initiale, sauf si les arguments adverses sont **clairement fondés** et plus convaincants.
 
-        Tu ne dois **pas changer d’avis à la légère**. Un changement de réponse ne doit se produire que si :
-        - L’argumentation adverse est **factuellement correcte**,
-        - Elle met en évidence une **erreur explicite** ou une **faiblesse importante** dans ta propre justification.
+        Dans tous les cas, justifie soigneusement ta décision.
 
-        Dans tous les cas, justifie soigneusement ta décision, en soulignant **pourquoi tu maintiens ou modifies ta position.**
-
-        Ta réponse doit être au format JSON suivant :
+        Réponds au format JSON :
         {{
             "classe": "...",
             "justification": "..."
         }}
-        Soit bref dans ta justification.
-
+        Sois bref dans ta justification.
         """
-        response = extract_json(debater2.invoke(prompt))
+        response = extract_json(debater2.predict(input=prompt))
         logger.info(f"Réponse du classificateur 2 ({name_debater2}) : {response}\n")
-    print("Debator 2 :", response)
-    return {"debater2_response": response }
+
+    print("Debater 2 :", response)
+    return {"debater2_response": response}
 
 def end(state: State):
     if state['agreement'] == True or (state['turn'] == MAX_TURN and state['agreement'] == False):
@@ -175,41 +172,47 @@ def end(state: State):
     else:
         return ["debater1", "debater2"]
 
-
 def END_fnc(state: State):
     if state['turn'] == MAX_TURN and state['agreement'] == False:
         prompt = f"""
         Voici les avis finaux des classificateurs :
         classificateur 1 : {state['debater1_response']}.
         classificateur 2 : {state['debater2_response']}.
-        Vous êtes l'orchestrateur.
         Le débat a atteint le nombre maximal de tours ({MAX_TURN}).
         Résumez le débat et donnez une conclusion.
-        Si consensus n'a pas été atteint entre les classificateurs alors tranche entre eux et donne la bonne classe et une seule en produisant la justification adéquate.
-        Veuille à me fournir ta reponse dans le format json suivant :
+
+        Tranche entre les deux si nécessaire.
+
+        Format attendu :
         {{
             "classe" : "...",
             "justification": "..."
         }}
         """
-    if state['agreement'] == True:
+    else:
         prompt = f"""
         Voici les avis finaux des classificateurs :
         classificateur 1 : {state['debater1_response']}.
         classificateur 2 : {state['debater2_response']}.
         Un consensus a été atteint.
-        Fournissez la reponse sous ce format, veuille bien à me retourner qu'une seule classe : 
-         {{
+
+        Format attendu :
+        {{
             "classe" : "...",
             "justification": "..."
         }}
-  
         """
-    final_evaluation = extract_json(orchestrator.invoke(prompt))
+    final_evaluation = extract_json(orchestrator.predict(input=prompt))
     logger.info(f"Résumé final du débat par l'orchestrateur ({name_orchestrator}) : {final_evaluation}\n")
-    #clean buffer
+
+    # Clean memory
+    memory_orchestrator.clear()
+    memory_debater1.clear()
+    memory_debater2.clear()
+
     return {"agreement": True, "turn": state['turn'], "final_evaluation": final_evaluation}
 
+# Build the LangGraph
 graph_builder = StateGraph(State)
 graph_builder.add_node("Orchestrator", Orchestrator)
 graph_builder.add_node("debater1", Debater1)
@@ -219,10 +222,11 @@ graph_builder.add_node("END", END_fnc)
 graph_builder.add_edge(START, "Orchestrator")
 graph_builder.add_edge("debater1", "Orchestrator")
 graph_builder.add_edge("debater2", "Orchestrator")
-graph_builder.add_conditional_edges("Orchestrator", end,["debater1","debater2", "END"])
+graph_builder.add_conditional_edges("Orchestrator", end, ["debater1", "debater2", "END"])
+
 graph = graph_builder.compile()
 
-if __name__=="__main__":
+if __name__ == "__main__":
     img = graph.get_graph().draw_mermaid_png()
     with open("graph.png", "wb") as f:
         f.write(img)
---
+++ res
@@ -0,0 +1,3 @@
+debat_hybrid_no_memories:
+orchestrator mistral : 0.67 acc
+
